87a9fe91b2c04d75b9dc42dcb5711ec6a9daa0cf
Amaury Deltenre <amaury.deltenre@gmail.com>
2020-05-27 11:22:47 +0200

    Modification SCSS and Display gallerie

--- wp-content/themes/foodog/footer.php
+++ wp-content/themes/foodog/footer.php
@@ -8,7 +8,7 @@
         <footer>
             <?php wp_nav_menu(['theme_location' => 'footer', 'container' => false, 'menu_class' => 'foodog-menu-footer', 'fallback_cb' => false]) ?>
             <div class="foodog-footer-row row">
-                <div class="footer-first offset-1 col-md-2 text-center text-decoration-none">
+                <div class="footer-first offset-1 col-md-2 text-center">
                     <?php if (is_active_sidebar('first_footer')) { ?>
                         <?php dynamic_sidebar('first_footer'); ?>
                     <?php } ?>
@@ -25,12 +25,12 @@
                     <?php } ?>
                 </div>
             </div>
-            <div class="footer-fourth bg-dark text-white text-center text-top pb-5 pt-3">
+            <div class="footer-fourth text-white text-center text-top pb-5 pt-3">
                 <div class="foodog-footericon pl-4">
-                    <a class="btn-floating btn-lg btn-fb mr-2 offset-6 bg-secondary rounded-circle" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn-floating btn-lg btn-fb mr-2 offset-6 bg-secondary" type="button" role="button"><i class="fab fa-facebook-f"></i></a>
 
-                    <a class="btn-floating btn-lg btn-tw mr-1 bg-secondary rounded-circle" type="button" role="button"><i class="fab fa-twitter"></i></a>
-                    <a class="btn-floating btn-lg btn-ins mr-1 bg-secondary rounded-circle" type="button" role="button"><i class="fab fa-instagram"></i></a>
+                    <a class="btn-floating btn-lg btn-tw mr-1 bg-secondary " type="button" role="button"><i class="fab fa-twitter"></i></a>
+                    <a class="btn-floating btn-lg btn-ins mr-1 bg-secondary" type="button" role="button"><i class="fab fa-instagram"></i></a>
                 </div>
             </div>
         </footer>
