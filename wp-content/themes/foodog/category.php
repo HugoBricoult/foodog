8f7c534ead0a86a4ba18cf592c7cddd70279781c
Kevin Pirnay <kevinpirnay@MacBook-Air-de-Kevin.local>
2020-05-29 09:32:49 +0200

    push category.php

--- wp-content/themes/foodog/category.php
+++ wp-content/themes/foodog/category.php
@@ -1,21 +1,16 @@
-<?php get_header() ?>
-<div class="container">
-<?php if(have_posts()){
-    while(have_posts()){
-        the_post(); ?>
-        <div class="container d-flex flex-row">
-            <img src="<?php the_post_thumbnail_url() ?>" alt="">
-            <div class="container d-flex flex-column">
-                <?php the_category() ?>
-                <br>
-                <?php the_title() ?>
-                <br>
-                <?php the_content() ?>
-                <br>
-                
-            </div>
-        </div>
-    <?php }
-}?>
-</div>
-<?php get_footer() ?>
+<?php
+
+require 'navbar.php';
+require 'toolFunction.php';
+
+get_header();
+
+display_heading(ret_the_category(" "));
+
+display_article();
+
+foodog_pagination();
+
+include('footer-basic.php');
+
+get_footer();
